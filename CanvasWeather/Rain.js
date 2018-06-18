bf229efcaa503b50dd2f0d1388b3cb9459032732
saleach <Sanford.Leach@turner.com>
2018-06-18 11:31:18 -0400

    Fixed Rain

--- CanvasWeather/Rain.js
+++ CanvasWeather/Rain.js
@@ -1,10 +1,8 @@
 let particles = [];
 
 function initRain() {
-     
-    canvas.strokeStyle = 'rgba(174,194,224,0.5)';
-    canvas.lineWidth = 1;
-    canvas.lineCap = 'round';
+    context.strokeStyle = '#8398AD';
+    context.lineCap = 'round';
 
     let init = [];
     let maxParts = 1000;
@@ -14,8 +12,9 @@ function initRain() {
             x: Math.random() * canvas.width,
             y: Math.random() * canvas.height,
             len: Math.random() * 1,
+            width: Math.random() * 2,
             xs: -4 + Math.random() * 4 + 2,
-            ys: Math.random() * 10 + 10
+            ys: Math.random() * 10 + 15
         })
     }
 
@@ -25,33 +24,21 @@ function initRain() {
 }
 
 function drawRain() {
-    requestAnimationFrame(drawRain);
-
-    // Set NOW and DELTA
-    now = Date.now()
-    delta = now - then;
-
-    // New frame
-    if (delta > interval) {
-        then = now - (delta % interval);
-        canvas.clearRect(0, 0, canvas.width, canvas.height);
-        
-        for (let i = 0; i < particles.length; i++) {
-            let p = particles[i]
-            canvas.beginPath();
-            canvas.moveTo(p.x, p.y);
-            canvas.lineTo(p.x + p.len * p.xs, p.y + p.len * p.ys);
-            canvas.stroke();
-        }
-
-        moveRain();
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    
+    for (let i = 0; i < particles.length; i++) {
+        let p = particles[i]
+        context.lineWidth = p.width;
+        context.beginPath();
+        context.moveTo(p.x, p.y);
+        context.lineTo(p.x + p.len * p.xs, p.y + p.len * p.ys);
+        context.stroke();
     }
+
+    moveRain();
 }
 
 function moveRain() {
-
-    requestAnimationFrame(moveRain);
-
     for (let i = 0; i < particles.length; i++) {
         let p = particles[i];
         p.x += p.xs;
