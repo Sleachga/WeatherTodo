cc0f3b13c70e000230ca354ca6af1900d502479b
saleach <Sanford.Leach@turner.com>
2018-06-26 14:32:53 -0400

    Sunny is almost complete

--- Sun.js
+++ Sun.js
@@ -1,22 +1,30 @@
 function initSun() {
     $("body").css("background-color", "#7EC6F6");
+    
+    sunPos = { 
+        x: (5/6) * canvas.width,
+        y: (1/6) * canvas.height
+    };
+
+    rayLen = Math.hypot(sunPos.x, canvas.height - sunPos.y);
 }
 
 const smallestRadius = 50;
 let curRadius = smallestRadius;
 let add = true;
+let sunPos = {};
 
 function drawSun() {
-    if (add) curRadius+= 0.05;
-    else curRadius-= 0.05;
+    if (add) curRadius+= 0.03;
+    else curRadius-= 0.03;
 
-    if (curRadius > smallestRadius + 30) add = false;
+    if (curRadius > smallestRadius + 20) add = false;
     if (curRadius < smallestRadius) add = true;
 
     // Fill the circle
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.beginPath();
-    context.arc((5/6) * canvas.width, (1/6) * canvas.height, curRadius, 0, 2 * Math.PI);
+    context.arc(sunPos.x, sunPos.y, curRadius, 0, 2 * Math.PI);
     context.fillStyle = "rgba(255, 255, 255, 1)";
     context.fill();
 
@@ -27,4 +35,29 @@ function drawSun() {
     context.shadowOffsetX = 0;
     context.shadowOffsetY = 0;
     context.stroke();
+
+    drawSunRays();
+}
+
+// Find the distance from center of sun to bottom left corner (furthest corner)
+// Draw a ray to there and rotate it
+
+class sunray {
+    constructor(angle) {
+        this.angle = angle;
+    }
+}
+
+let rayLen = 0;
+let sunrays = [0, 45, 90, 135, 180, 225, 270, 315];
+
+function drawSunRays() {
+    for (let i = 0; i < sunrays.length; i++) {
+        context.beginPath();
+        context.moveTo(sunPos.x, sunPos.y);
+        context.lineTo(sunPos.x + rayLen * Math.cos(Math.PI * sunrays[i] / 180), 
+        sunPos.y + rayLen * Math.sin(Math.PI * sunrays[i] / 180));
+        context.stroke();
+        sunrays[i]+=0.1;
+    }
 }
