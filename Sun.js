5a08feb847a9812e972fc0a0026935f51feb25a8
saleach <Sanford.Leach@turner.com>
2018-06-25 23:39:59 -0400

    Added sun glow

--- Sun.js
+++ Sun.js
@@ -13,9 +13,18 @@ function drawSun() {
     if (curRadius > smallestRadius + 30) add = false;
     if (curRadius < smallestRadius) add = true;
 
+    // Fill the circle
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.beginPath();
     context.arc((5/6) * canvas.width, (1/6) * canvas.height, curRadius, 0, 2 * Math.PI);
     context.fillStyle = "rgba(255, 255, 255, 1)";
     context.fill();
+
+    // Make the shadow
+    context.strokeStyle = "#ffffff";
+    context.shadowColor = '#ffffff';
+    context.shadowBlur = 30;
+    context.shadowOffsetX = 0;
+    context.shadowOffsetY = 0;
+    context.stroke();
 }
